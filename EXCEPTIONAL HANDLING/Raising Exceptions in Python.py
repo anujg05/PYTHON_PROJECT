fdf21a42b5601b785de2cb1c2a771bda1ce76fcb
anujg05 <106540285+anujg05@users.noreply.github.com>
2022-05-31 22:01:02 +0530

    Create Raising Exceptions in Python.py

---
+++ EXCEPTIONAL HANDLING/Raising Exceptions in Python.py
@@ -0,0 +1,19 @@
+>>> raise KeyboardInterrupt
+Traceback (most recent call last):
+...
+KeyboardInterrupt
+
+>>> raise MemoryError("This is an argument")
+Traceback (most recent call last):
+...
+MemoryError: This is an argument
+
+>>> try:
+...     a = int(input("Enter a positive integer: "))
+...     if a <= 0:
+...         raise ValueError("That is not a positive number!")
+... except ValueError as ve:
+...     print(ve)
+...    
+Enter a positive integer: -2
+That is not a positive number
